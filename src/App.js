33582a269d61911600635ceb4c627a1f44a6b28e
anonymous <anonymous>
2019-08-21 17:05:47 -0400

    CORS error overcome

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import People from './components/People'
 
+
 function App() {
   return (
     <>
--- src/components/People.js
+++ src/components/People.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 
-
-
 class People extends Component {
     state={
         people: []
     }
 
 getPeople = async () => {
-    const API_KEY = process.env.API_KEY
-    const peopleResponse = await axios.get(`https://developers.salesloft.com/${API_KEY}/People/get_v2_people_json`)
-    this.setState({
-        people: peopleResponse.data
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const peopleResponse = await axios.get('https://cors-anywhere.herokuapp.com/https://api.salesloft.com/v2/people.json', {
+        headers: {
+            Authorization: 'Bearer ' + API_KEY
+        }
     })
+    this.setState({ people: peopleResponse.data.data })
 }
 
 componentDidMount() {
     this.getPeople()
+    console.log()
 }
 
     render() {
